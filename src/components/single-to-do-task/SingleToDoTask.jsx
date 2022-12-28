808313f456b781e9d3e3002f61693ad986ace9b8
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2022-12-28 22:47:09 +0530

    Add files via upload

---
+++ src/components/single-to-do-task/SingleToDoTask.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {FaCheckCircle, FaTrashAlt} from 'react-icons/fa'
+
+const SingleToDoTask = ({id, task, list, setList}) => {
+
+    const [taskComplete, setTaskComplete] = React.useState(false)
+
+    // handle the deletion of task
+    const handleDelete = (id) => {
+        const filteredList = list.filter(item => item.id !== id)
+        setList(filteredList);
+    }
+
+    return (
+        <div key={id} id={id} className="single-task">
+            <span style={taskComplete ? {textDecoration:'line-through'}: {}}>{task}</span>
+            <button title='Task complete' onClick={() => setTaskComplete(prev => !prev)}><FaCheckCircle /></button>
+            <button title='Delete task' onClick={() => handleDelete(id)}><FaTrashAlt /></button>
+        </div>
+    )
+}
+
+export default SingleToDoTask
