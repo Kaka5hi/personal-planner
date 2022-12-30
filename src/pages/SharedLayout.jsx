a2b7bed3a271c250bd400740e1e513be08cd15eb
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2022-12-30 23:47:10 +0530

    Routes, files and dynamic style updated

--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -4,22 +4,26 @@ import {MdDashboard, MdOutlineSettings} from 'react-icons/md'
 import {FaTasks} from 'react-icons/fa'
 import {RiTodoLine} from 'react-icons/ri'
 import {TbBusinessplan} from 'react-icons/tb'
+import {RxDoubleArrowRight , RxCross2} from 'react-icons/rx'
 
 
 const Sharedlayout = () => {
 
+    const [show, setShow] = React.useState(true)
+
     return (
-        <div className='main_container'>
-            <header>
+        <div className='main-container'>
+            <header style={ show ? {width:'18vw'} : {width:'6vw'}}>
                 <nav>
                     <ul className='sidebar_links'>
-                        <li><NavLink to='/'>< MdDashboard />Dashboard</NavLink></li>
-                        <li><NavLink to='/tasks'><FaTasks />Task</NavLink></li>
-                        <li><NavLink to='/notes'><RiTodoLine />Notes</NavLink></li>
-                        <li><NavLink to='/budget'><TbBusinessplan />Budget</NavLink></li>
-                        <li><NavLink to='/setting'>< MdOutlineSettings />Setting</NavLink></li>
+                        <li title='Dashboard' ><NavLink to='/'>< MdDashboard />{show ? 'Dashboard' : ''}</NavLink></li>
+                        <li title='Project'><NavLink to='/project'><FaTasks />{show ? 'Project' : ''}</NavLink></li>
+                        <li title='Notes'><NavLink to='/notes'><RiTodoLine />{show ? 'Notes' : ''}</NavLink></li>
+                        <li title='Budget'><NavLink to='/budget'><TbBusinessplan />{show ? 'Budget' : ''}</NavLink></li>
+                        <li title='Setting'><NavLink to='/setting'>< MdOutlineSettings />{show ? 'Setting' : ''}</NavLink></li>
                     </ul>
                 </nav>
+                <button title='Toggle Sidebar' onClick={() => setShow(prev=>!prev) }>{show ? <RxCross2 /> : <RxDoubleArrowRight /> }</button>
             </header>
             <main>
                 <Outlet />
