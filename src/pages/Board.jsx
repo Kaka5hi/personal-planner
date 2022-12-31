01d97367127723b2ad75cf0aa8249f0c7c3a899e
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2022-12-31 18:55:58 +0530

    Add files via upload

---
+++ src/pages/Board.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Board = () => {
+    return (
+        <div className='page-container'>
+            <div className="board_inner-container">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Board
