7f7f32d28e81c20c465f07c1fadedd21d0a17bf2
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2022-12-31 19:00:08 +0530

    Routing for unique board added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Project from './pages/Project'
+import Board from './pages/Board'
 import Budget from './pages/Budget'
 import Notes from './pages/Notes'
 import Setting from './pages/Setting'
@@ -16,6 +17,7 @@ const App = () => {
                 <Route path='/' element={<Sharedlayout />}>
                     <Route index element={<Home />} />
                     <Route path='/project' element={<Project />} />
+                    <Route path='/project/board/:id' element={<Board />} />
                     <Route path='/budget' element={<Budget />} />
                     <Route path='/notes' element={<Notes />} />
                     <Route path='/setting' element={<Setting />} />
