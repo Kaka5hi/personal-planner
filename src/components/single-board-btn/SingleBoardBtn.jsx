20e0d29b43d4b520a777bdd9b3330b6677c00b5a
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2022-12-31 18:59:24 +0530

    Update SingleBoardBtn.jsx

--- src/components/single-board-btn/SingleBoardBtn.jsx
+++ src/components/single-board-btn/SingleBoardBtn.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {FaExternalLinkAlt, FaTrashAlt} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const SingleBoardBtn = () => {
     return (
         <div className="create_board-item_container">
             <h1>Board name</h1>
             <div className="btn-container">
-                <button title='Go to Board'><FaExternalLinkAlt /></button>
+                <Link to='/project/board/2' title='Go to Board'><FaExternalLinkAlt /></Link>
                 <button title='Delete Board'><FaTrashAlt /> </button>
             </div>
 
