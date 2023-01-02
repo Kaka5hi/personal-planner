4243934f3d780cb0a324f6e07335816dfbde4937
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-02 22:14:39 +0530

    Update Project.jsx

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -5,6 +5,24 @@ import './Project.css'
 const Project = () => {
 
     const [showPopupModal, setShowPopupModal] = React.useState(false)
+    const [boardName, setBoardName] = React.useState('')
+    const [boards, setBoards] = React.useState([])
+
+
+    const handleBoardName = (e) => {
+        e.preventDefault()
+        if(boardName) {
+
+            const newBoard = {id: new Date().getTime().toString(), boardName: boardName}
+            setBoards(prev => [
+            ...prev,
+            newBoard
+        ]);
+        setShowPopupModal(false)
+        } else {
+            alert("Enter a board name, You can not leave board name empty")
+        }
+    }
 
     return (
         <div className='page-container'>
@@ -14,7 +32,7 @@ const Project = () => {
                 </div>
 
                 {/* Here we will add board containers when user click on create card and give the board name */}
-
+                {boards.map(item => <SingleBoardBtn key={item.id} item={item} />)}
             </div>
             
             {showPopupModal && <div className="create_board-popup_modal">
@@ -23,16 +41,18 @@ const Project = () => {
                         <h4>New Board</h4>
                     </div>
                     <div className="middle">
-                        <form>
+                        <form onSubmit={handleBoardName}>
                             <input 
+                                autoFocus
                                 type="text"
                                 placeholder='Enter a name for the Board'
+                                onChange={(e) => setBoardName(e.target.value)}
                             />
                         </form>
                     </div>
                     <div className="bottom">
                         <button onClick={() => setShowPopupModal(false)} >Cancel</button>
-                        <button>Create</button>
+                        <button onClick={handleBoardName}>Create</button>
                     </div>
                 </div>
             </div>}
