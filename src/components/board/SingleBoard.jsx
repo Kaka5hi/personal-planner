454021a17bf67aad4024b4cf3e196ef79ad2f4ba
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-02 22:13:05 +0530

    Add files via upload

---
+++ src/components/board/SingleBoard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SingleBoard.css'
+
+const SingleBoard = () => {
+    return (
+        <div>
+            Single board
+        </div>
+    )
+}
+
+export default SingleBoard
