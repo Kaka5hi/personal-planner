5486ef993859c24f7da2ded210adfcdef5d64c14
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-03 15:46:39 +0530

    Update SingleBoardBtn.jsx

--- src/components/single-board-btn/SingleBoardBtn.jsx
+++ src/components/single-board-btn/SingleBoardBtn.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import {FaExternalLinkAlt, FaTrashAlt} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
-const SingleBoardBtn = () => {
-    return (
-        <div className="create_board-item_container">
-            <h1>Board name</h1>
-            <div className="btn-container">
-                <Link to='/project/board/2' title='Go to Board'><FaExternalLinkAlt /></Link>
-                <button title='Delete Board'><FaTrashAlt /> </button>
-            </div>
+const SingleBoardBtn = ({item, boards, setBoards}) => {
 
+    const handleBoardDelete = (id) => {
+        const filteredList = boards.filter(item => item.id !== id)
+        setBoards(filteredList);
+        localStorage.setItem('boardList', JSON.stringify(filteredList))
+    }
+
+    return (
+        <div id={item.id} className="create_board-item_container">
+            <h1>{item.boardName}</h1>
+            <div className="btn-container">
+                <Link to={`/project/board/${item.boardName}/${item.id}`} title='Go to Board'><FaExternalLinkAlt /></Link>
+                <button onClick={() => handleBoardDelete(item.id)} title='Delete Board'><FaTrashAlt /> </button>
+            </div>
         </div>
     )
 }
