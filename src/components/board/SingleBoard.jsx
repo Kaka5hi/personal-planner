f8320820ba6897ac11d337c9cb7e9e887967c622
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-03 15:44:07 +0530

    Update SingleBoard.jsx

--- src/components/board/SingleBoard.jsx
+++ src/components/board/SingleBoard.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './SingleBoard.css'
+import { MdMoreHoriz } from 'react-icons/md'
+import Card from '../card/Card.jsx'
 
 const SingleBoard = () => {
     return (
-        <div>
-            Single board
+        <div className='board'>
+            <div className="board_heading">
+                <h4>Heading of the card <span>0</span></h4>
+                <MdMoreHoriz />
+            </div>
+            <div className="board_cards-container">
+                <Card />
+            </div>
         </div>
     )
 }
