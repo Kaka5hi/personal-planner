4473a054609445ece1ac84387de67fdff7e6ac41
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-03 15:48:13 +0530

    Update Board.jsx

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import SingleBoard from '../components/board/SingleBoard'
+import './Board.css'
 
 const Board = () => {
     const params = useParams()
 
     return (
-        <div className='page-container'>
-            <div className="board_top-container">
-                <h1 style={{textAlign:'center', textTransform:'capitalize'}}>{params.name}</h1>
-            </div>
-            <div className="board_inner-container">
-                <SingleBoard />
+        <div className='page-container' style={{paddingRight:0, paddingBottom: '5px'}}>
+            <h1 style={{textAlign:'center', textTransform:'capitalize'}}>{params.name}</h1>
+            <div className="board_outer-container">
+                <div className="board_inner-container">
+                    <SingleBoard />
+                    <SingleBoard />
+                    <SingleBoard />
+                    <SingleBoard />
+                </div>
             </div>
         </div>
     )
