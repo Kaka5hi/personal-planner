4a3404a0a8932294391f462d3761e31edcd76df3
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-04 21:53:04 +0530

    Update Card.jsx

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import LabelChips from '../label-chips/LabelChips'
+import { MdMoreHoriz, MdAccessTime } from 'react-icons/md'
+import {FaTasks} from 'react-icons/fa'
 import './Card.css'
+import PriorityChip from '../priority/PriorityChip'
 
 const Card = () => {
     return (
         <div className='card'>
             <div className="card_top">
                 <h4>Card heading</h4>
+                <MdMoreHoriz />
             </div>
             <div className="card_middle">
                 <LabelChips />
                 <LabelChips />
                 <LabelChips />
+                <LabelChips />
+                <PriorityChip />
             </div>
-            <div className="card_bottom"></div>
+            <div className="card_bottom">
+                <span>
+                    <MdAccessTime />
+                    due sep 12 (1 day left)
+                </span>
+                <span>
+                    <FaTasks />
+                    3/12 subtasks completed
+                </span>
+            </div>
         </div>
     )
 }
