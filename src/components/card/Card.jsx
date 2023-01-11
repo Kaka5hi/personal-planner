1543f4d0181e98ed848f31870996b1847e0a018a
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-11 23:03:26 +0530

    Update Card.jsx

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,19 +4,25 @@ import { MdMoreHoriz, MdAccessTime } from 'react-icons/md'
 import {FaTasks} from 'react-icons/fa'
 import './Card.css'
 import PriorityChip from '../priority/PriorityChip'
+import Dropdown from '../dropdown/Dropdown'
 
 const Card = () => {
+
+    const [dropdown, setDropdown] = React.useState(false)
+
     return (
-        <div className='card'>
+        <div style={{position:'relative'}} className='card'>
+            {dropdown && <Dropdown type={'card'}/>}
             <div className="card_top">
-                <h4>Card heading</h4>
-                <MdMoreHoriz />
+                <h4 style={{userSelect:'none'}}>Card heading</h4>
+                <MdMoreHoriz onClick={() => setDropdown(prev => !prev)}/>
             </div>
             <div className="card_middle">
-                <LabelChips />
-                <LabelChips />
-                <LabelChips />
-                <LabelChips />
+                <LabelChips text={"frontend"}/>
+                <LabelChips text={"ui"}/>
+                <LabelChips text={"react"}/>
+                <LabelChips text={"tailwind"}/>
+                <LabelChips text={"redux"}/>
                 <PriorityChip />
             </div>
             <div className="card_bottom">
