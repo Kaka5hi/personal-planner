426320e17466263a07c52b7ea15edcae33df5202
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-15 01:52:10 +0530

    Update PriorityChip.jsx

--- src/components/priority/PriorityChip.jsx
+++ src/components/priority/PriorityChip.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import {RiFlag2Line} from 'react-icons/ri'
 import './PriorityChip.css'
 
-const PriorityChip = () => {
+const PriorityChip = ({priority}) => {
     return (
         <div className='priority_chips'>
-             {/* check color pallet and refer to apple ios color pallet */}
-            <span>High <RiFlag2Line /></span> 
+            <span>{priority}<RiFlag2Line /></span> 
         </div>
     )
 }
