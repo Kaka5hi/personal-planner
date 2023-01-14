cfe7345529f5af839c087447ecd6f90b431a3f62
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-15 01:49:32 +0530

    Update Dropdown.jsx

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {FaTrashAlt} from 'react-icons/fa'
 import './Dropdown.css'
 
-const Dropdown = ({type, setCategoryList, id, categoryList}) => {
+const Dropdown = ({type, setCategoryList, id, categoryList, deleteCard, cardId, categoryId}) => {
     
     const handleCategoryDelete = (id) => {
         const filteredList = categoryList.filter(item => id !== item.category_id)
@@ -11,7 +11,7 @@ const Dropdown = ({type, setCategoryList, id, categoryList}) => {
 
     return (
         <div className='dropdown'>
-            <p onClick={() => handleCategoryDelete(id)}>Delete {type} <FaTrashAlt /></p>
+            <p onClick={type ==='category' ? () => handleCategoryDelete(id) : () => deleteCard(cardId, categoryId)}>Delete {type} <FaTrashAlt /></p>
         </div>
     )
 }
