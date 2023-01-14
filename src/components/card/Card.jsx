f790d960e780bd13ecb8c0303f36b870a2d49f1c
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-15 01:46:49 +0530

    Update Card.jsx

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,30 +6,35 @@ import './Card.css'
 import PriorityChip from '../priority/PriorityChip'
 import Dropdown from '../dropdown/Dropdown'
 
-const Card = () => {
+const Card = ({card, deleteCard, cardId, categoryId, handleDragEnter, handleDragEnd}) => {
 
     const [dropdown, setDropdown] = React.useState(false)
-
     return (
-        <div style={{position:'relative'}} className='card'>
-            {dropdown && <Dropdown type={'card'}/>}
+        <div style={{position:'relative'}} className='card' draggable 
+            onDragEnter={() => handleDragEnter(cardId, categoryId)} 
+            onDragEnd={() => handleDragEnd(cardId, categoryId)}
+        >
+            {dropdown && <Dropdown 
+                            type={'card'}
+                            deleteCard={deleteCard}
+                            cardId={cardId}
+                            categoryId={categoryId}
+                        />}
             <div className="card_top">
-                <h4 style={{userSelect:'none'}}>Card heading</h4>
+                <h4 style={{userSelect:'none'}}>{card.card_name}</h4>
                 <MdMoreHoriz onClick={() => setDropdown(prev => !prev)}/>
             </div>
             <div className="card_middle">
-                <LabelChips text={"frontend"}/>
-                <LabelChips text={"ui"}/>
-                <LabelChips text={"react"}/>
-                <LabelChips text={"tailwind"}/>
-                <LabelChips text={"redux"}/>
-                <PriorityChip />
+                {card?.card_labels.map((item, index) => <LabelChips key={index} text={item}/>)}
+                <PriorityChip priority={card?.card_priority} />
             </div>
             <div className="card_bottom">
-                <span>
-                    <MdAccessTime />
-                    due sep 12 (1 day left)
-                </span>
+                {card.card_date && 
+                    <span>
+                        <MdAccessTime />
+                        due sep 12 (1 day left)
+                    </span>
+                }
                 <span>
                     <FaTasks />
                     3/12 subtasks completed
