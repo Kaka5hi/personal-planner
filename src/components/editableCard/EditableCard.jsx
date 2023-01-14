8dd22ff21199b4881bdce8da726052ee65ec6d31
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-15 01:50:17 +0530

    Update EditableCard.jsx

--- src/components/editableCard/EditableCard.jsx
+++ src/components/editableCard/EditableCard.jsx
@@ -2,24 +2,52 @@ import React from 'react'
 import './EditableCard.css'
 import {AiOutlinePlus} from 'react-icons/ai'
 import CardDetails from '../cardDetails/CardDetails'
+import { useParams } from 'react-router-dom'
 
 
-const Editable = ({text}) => {
+const Editable = ({text, category, categoryId, setCategoryList, categoryList,}) => {
 
-    const [editableText, setEditableText] = React.useState('')
-    const [showEditable, setShowEditable] = React.useState(false)
+    const params = useParams()
+    // for new card popup
+    const [showEditableCard, setShowEditableCard] = React.useState(false)
 
-    const handleTaskSubmit = (e) => {
-        e.preventDefault()
-        setShowEditable(false)
-        setEditableText('')
+    const createNewCard = (cardDetails, categoryId) => {
+        const newCard = {
+            card_id:new Date().getTime() + Math.random(),
+            card_name: cardDetails?.card_name,
+            card_description: cardDetails?.card_description,
+            card_labels: cardDetails?.card_labels,
+            card_priority: cardDetails?.card_priority,
+            card_date: cardDetails?.card_date,
+            card_subtasks: cardDetails?.card_subtasks
+        }
+
+        const index = categoryList.findIndex(item => item.category_id === categoryId)
+        if(index < 0) {
+            return
+        }
+
+        const tempCategory = [...categoryList]
+        tempCategory[index].category_cards.push(newCard)
+        setCategoryList(tempCategory)
+        setShowEditableCard(false)
     }
 
+    React.useState(()=> {
+        localStorage.setItem(`${params.id}`, JSON.stringify(categoryList))
+    }, [categoryList])
+
+
     return (
-        <div className='editable'>
-            {showEditable
-                ?   <CardDetails setShowEditable={setShowEditable} />
-                :   <p onClick={() => setShowEditable(true)}>
+        <div className='editable_card'>
+            {showEditableCard
+                ?   <CardDetails 
+                        setShowEditableCard={setShowEditableCard} 
+                        createNewCard={createNewCard} 
+                        category={category}
+                        categoryId={categoryId}
+                    />
+                :   <p onClick={() => setShowEditableCard(true)}>
                         <AiOutlinePlus /> new {text}
                     </p> 
             }
