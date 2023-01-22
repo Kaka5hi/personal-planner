58a9db5f0c2923a38877923ca0286dc53524489a
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-22 18:25:59 +0530

    subtask check box feature and functionality added

--- src/components/finalCard/FinalCard.jsx
+++ src/components/finalCard/FinalCard.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import LabelChips from '../label-chips/LabelChips'
 import PriorityChip from '../priority/PriorityChip'
 import './FinalCard.css'
-import {FaTrashAlt} from 'react-icons/fa'
+import { FaTasks } from 'react-icons/fa'
+import { MdAccessTime } from 'react-icons/md'
+import Subtask from '../single-subtask/Subtask'
 
-const FinalCard = ({ setShowCardDetails, card }) => {
+const FinalCard = ({ setShowCardDetails, card, calculateDate, categoryId, updateSubtaskStatus }) => {
+
+    const progressBarWidth = () => {
+        const progress = card?.card_subtasks?.filter(item => item?.subtask_status === true).length / card?.card_subtasks.length
+        if (progress === NaN) {
+            return 0
+        } else {
+            return (progress * 100);
+        }
+    }
+  
     return (
         <div className='final_card'>
             <div className="final_card-inner_container">
@@ -17,27 +29,40 @@ const FinalCard = ({ setShowCardDetails, card }) => {
                     <PriorityChip priority={card?.card_priority} />
                 </div>
                 <div className="bottom">
-                    <span>Subtasks</span>
+                    {card?.card_date &&
+                        <span className='date'>
+                            <MdAccessTime />
+                            due {calculateDate()}
+                        </span>
+                    }
+                    {card?.card_subtasks.length !==0 && 
+                        <span className='subtask'>
+                            <FaTasks />
+                            {card?.card_subtasks?.filter(item => item?.subtask_status === true).length}/{card?.card_subtasks.length} subtasks completed
+                        </span>
+                    }
+                    <span style={{ fontWeight: 500 }} >{card?.card_subtasks.length === 0 ? '' : 'Subtasks'}</span>
                     {
                         card?.card_subtasks?.length !== 0 &&
                         <div className="subtask_progress-bar_container">
-                            <div className="subtask_progress-bar" style={{ width: '50%' }}></div>
+                            <div className="subtask_progress-bar" style={{ width:`${progressBarWidth()}%`, background: `${progressBarWidth() === 100 ? '#4cd964':'#72a0c9'}`}}></div>
                         </div>
                     }
                     <div className='card_subtask_container'>
-                        {card?.card_subtasks?.map(task => {
-                            return (
-                                <div key={task?.subtask_id} className='card_single-subtask'>
-                                    <input type="checkbox"/>
-                                    <p>{task?.subtask_name}</p>
-                                    <FaTrashAlt />
-                                </div>
-                            )
-                        })
+                        {
+                            card?.card_subtasks?.map(task => <Subtask
+                                key={task?.subtask_id}
+                                subtaskId={task?.subtask_id}
+                                subtaskName={task?.subtask_name}
+                                subtaskStatus={task?.subtask_status}
+                                cardId={card?.card_id}
+                                categoryId={categoryId}
+                                updateSubtaskStatus={updateSubtaskStatus}
+                            />)
                         }
                     </div>
                 </div>
-            <button onClick={() => setShowCardDetails(true)}>close</button>
+            <button title='Close card details' onClick={() => setShowCardDetails(true)}>close</button>
             </div>
         </div>
     )
