f75363eea3bb5c4e00017f00f4b9558547d1712a
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-29 01:36:58 +0530

    Update Budget.jsx

--- src/pages/Budget.jsx
+++ src/pages/Budget.jsx
@@ -1,58 +1,85 @@
 import React from 'react'
-import { FiPlus } from 'react-icons/fi'
-import { FaTrashAlt } from 'react-icons/fa'
-import { MdCreate } from 'react-icons/md'
+import BudgetCategory from '../components/budget-category/BudgetCategory'
+import DonutChart from '../components/donut-chart/DonutChart'
+import TransactionList from '../components/transaction-list/TransactionList'
+import Transaction from '../components/transaction/Transaction'
+import './Budget.css'
 
-import './BudgetCategory.css'
+const Budget = () => {
+    //category input
+    const categoryRef = React.useRef("")
 
-const BudgetCategory = ({ createCategory, categoryRef, categoryList, noCategoryNameSubmit, setCategoryList, showTransactionPopup}) => {
-    
-    const handleDeleteBudgetCategory = (id) => {
-        const newList = categoryList.filter(item => item.id !== id)
-        setCategoryList(newList)
+    //budget category list 
+    const [categoryList, setCategoryList] = React.useState([])
+
+    //for warning/popup text if user submit catgeory name empty
+    const [noCategoryNameSubmit, setNoCategoryNameSubmit] = React.useState(false)
+
+    //for transaction form toggling
+    const [goToTransaction, setGotToTransaction] = React.useState(false)
+
+    // for selected current category
+    const [currentCategory, setCurrentCurrentCategory] = React.useState('')
+
+    // transaction list for displaying every transaction
+    const [transactionList, setTransactionList] = React.useState([])
+
+    //function will create new categorty in category secion
+    const createCategory = (e) => {
+        e.preventDefault()
+        const categoryName = categoryRef.current.value
+        if (categoryName === "") {
+            setNoCategoryNameSubmit(true)
+        } else {
+            setCategoryList(prev => [
+                ...prev, {categoryName, id: new Date().getTime()}
+            ])
+            categoryRef.current.value = ""
+            setNoCategoryNameSubmit(false)
+        }
     }
 
+    const showTransactionPopup = (currentCategory) => {
+        setGotToTransaction(true)
+        setCurrentCurrentCategory(currentCategory)
+    }
+
+    React.useEffect(() => {
+        localStorage.setItem("transaction-list", JSON.stringify(transactionList))
+        localStorage.setItem("transaction-category", JSON.stringify(categoryList))
+    }, [transactionList, categoryList])
+
     return (
-        <div className="budget_category">
-            <h3>expense category</h3>
-            <span>create expense categories like: Food, Rent, Saving etc.</span>
-            <form onSubmit={createCategory}>
-                <input
-                    type="text"
-                    placeholder="category name"
-                    ref={categoryRef}
-                />
-                <button type="submit">
-                    <FiPlus />
-                </button>
-            </form>
-            {
-                noCategoryNameSubmit
-                &&
-                <span className="budget_category_warning">Name cannot be empty</span>
-            }
-            <div className="category_chips-container">
-                {
-                    categoryList?.map(item => {
-                    return (
-                        <span key={item?.id} className="category_chips">
-                            {item.categoryName}
-                            <div className="chips_button">
-                                <FaTrashAlt
-                                    title="Delete Category"
-                                    onClick={() => handleDeleteBudgetCategory(item?.id)}
-                                    />
-                                <MdCreate
-                                    title="Go to transaction"
-                                    onClick={()=> showTransactionPopup(item?.categoryName)}
-                                    />
-                            </div>
-                        </span>
-                    )})
-                }
+        <div className='page-container' style={{position: 'relative'}}>
+            <h1 style={{ textAlign: 'center' }}>Budget</h1>
+            <div className="budget_inner-container">
+                <div className="budget_top">
+                    <DonutChart />
+                    <BudgetCategory
+                        createCategory={createCategory}
+                        categoryRef={categoryRef}
+                        noCategoryNameSubmit={noCategoryNameSubmit}
+                        categoryList={categoryList}
+                        setCategoryList={setCategoryList}
+                        showTransactionPopup={showTransactionPopup}
+                    />
+                </div>
             </div>
+            {
+                goToTransaction
+                &&
+                <Transaction
+                    currentCategory={currentCategory}
+                    setGotToTransaction={setGotToTransaction}
+                    transactionList={transactionList}
+                    setTransactionList={setTransactionList}
+                />
+            }
+            {
+                transactionList?.map(item => <TransactionList key={item?.id}/>)
+            }
         </div>
     )
 }
 
-export default BudgetCategory
+export default Budget
