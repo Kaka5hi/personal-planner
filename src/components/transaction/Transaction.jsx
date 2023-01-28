b60a447d4801db5aff2f3c027375a0974d976ba2
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-29 01:37:44 +0530

    Update Transaction.jsx

--- src/components/transaction/Transaction.jsx
+++ src/components/transaction/Transaction.jsx
@@ -1,11 +1,84 @@
 import React from 'react'
+import { MdOutlineClose } from 'react-icons/md'
+import './Transaction.css'
 
-const Transaction = () => {
+const Transaction = ({setGotToTransaction, currentCategory,transactionList, setTransactionList}) => {
+
+    const [transactionAmount, setTransactionAmount] = React.useState({
+        currentCategory,
+        amount: "",
+        date: "",
+        note:""
+    })
+
+    const handleTransactionSubmit = (e) => {
+        e.preventDefault()
+        const temp = {...transactionAmount, id: new Date().getTime()}
+        setTransactionList(prev => [
+            ...prev,
+            temp
+        ]);
+        setGotToTransaction(false)
+    }
+
     return (
         <div className="transaction">
             <div className="transaction_inner">
-                <p>show</p>
-                <button onClick={() => setGotToTransaction(false)} >close</button>
+                <div className="top">
+                    <p>{currentCategory}</p>
+                    <MdOutlineClose
+                        onClick={() => setGotToTransaction(false)}
+                    />
+                </div>
+                <div className="bottom">
+                    <form onSubmit={handleTransactionSubmit}>
+                        <label htmlFor="amount">amount</label>
+                        <input
+                            type="text"
+                            placeholder="Enter the amount"
+                            id="amount"
+                            value={transactionAmount.amount}
+                            autoComplete="off"
+                            onChange={(e) => setTransactionAmount(prev => {
+                                return {
+                                    ...prev,
+                                    amount:e.target.value
+                                }
+                            })}
+                        />
+                        <label htmlFor="date">pick date</label>
+                        <input
+                            id="date"
+                            type="date"
+                            placeholder="Select date"
+                            value={transactionAmount.date}
+                            autoComplete="off"
+                            onChange={(e) => setTransactionAmount(prev => {
+                                return {
+                                    ...prev,
+                                    date:e.target.value
+                                }
+                            })}
+                        />
+                        <label htmlFor="note">note</label>
+                        <input
+                            id="note"
+                            type="text"
+                            placeholder="Note"
+                            value={transactionAmount.note}
+                            autoComplete="off"
+                            onChange={(e) => setTransactionAmount(prev => {
+                                return {
+                                    ...prev,
+                                    note:e.target.value
+                                }
+                            })}
+                        />
+                        <button type="submit">
+                            add
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     )
