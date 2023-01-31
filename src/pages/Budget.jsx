6dd4d9fb2e5ecf9ac689cd429c544e3f8c539ac7
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-31 06:41:33 +0530

    pie chart added

--- src/pages/Budget.jsx
+++ src/pages/Budget.jsx
@@ -9,11 +9,40 @@ const Budget = () => {
     //category input
     const categoryRef = React.useRef("")
 
+    // dummy chart data
+    const data = {
+        datasets: [
+            {
+                label: 'total expense',
+                data: [],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1,
+                hoverOffset: 4,
+            },
+        ],
+        labels: [],
+    }
+
     //budget category list 
-    const [categoryList, setCategoryList] = React.useState([])
+    const [categoryList, setCategoryList] = React.useState(JSON.parse(localStorage.getItem("transaction-category")) || [])
 
     //for warning/popup text if user submit catgeory name empty
-    const [noCategoryNameSubmit, setNoCategoryNameSubmit] = React.useState(false)
+    const [noCategoryWarning, setNoCategoryWarning] = React.useState({warning:false, msg:""})
 
     //for transaction form toggling
     const [goToTransaction, setGotToTransaction] = React.useState(false)
@@ -22,20 +51,29 @@ const Budget = () => {
     const [currentCategory, setCurrentCurrentCategory] = React.useState('')
 
     // transaction list for displaying every transaction
-    const [transactionList, setTransactionList] = React.useState([])
+    const [transactionList, setTransactionList] = React.useState(JSON.parse(localStorage.getItem("transaction-list")) || [])
+
+    const [chartData, setChartData] = React.useState(JSON.parse(localStorage.getItem("chart-data")) || data)
 
     //function will create new categorty in category secion
     const createCategory = (e) => {
         e.preventDefault()
         const categoryName = categoryRef.current.value
+        const isCategoryAlreadyPresent = categoryList.find(item => item.categoryName === categoryName)
         if (categoryName === "") {
-            setNoCategoryNameSubmit(true)
+            setNoCategoryWarning({warning:true, msg:"Name cannot be empty"})
+        } else if (isCategoryAlreadyPresent) {
+            setNoCategoryWarning({warning:true, msg:"Category already exist"})
+            categoryRef.current.value = ""
         } else {
             setCategoryList(prev => [
                 ...prev, {categoryName, id: new Date().getTime()}
             ])
+            let tempChartData = { ...chartData }
+            tempChartData.labels.push(categoryName)
+            setChartData(tempChartData)
             categoryRef.current.value = ""
-            setNoCategoryNameSubmit(false)
+            setNoCategoryWarning({ warning: false, msg: "" })
         }
     }
 
@@ -47,37 +85,54 @@ const Budget = () => {
     React.useEffect(() => {
         localStorage.setItem("transaction-list", JSON.stringify(transactionList))
         localStorage.setItem("transaction-category", JSON.stringify(categoryList))
-    }, [transactionList, categoryList])
+        localStorage.setItem("chart-data", JSON.stringify(chartData))
+    }, [transactionList, categoryList, chartData])
 
     return (
         <div className='page-container' style={{position: 'relative'}}>
             <h1 style={{ textAlign: 'center' }}>Budget</h1>
             <div className="budget_inner-container">
                 <div className="budget_top">
-                    <DonutChart />
+                    <DonutChart
+                        data={chartData}
+                    />
                     <BudgetCategory
                         createCategory={createCategory}
                         categoryRef={categoryRef}
-                        noCategoryNameSubmit={noCategoryNameSubmit}
+                        noCategoryWarning={noCategoryWarning}
                         categoryList={categoryList}
                         setCategoryList={setCategoryList}
                         showTransactionPopup={showTransactionPopup}
+                        chartData={chartData}
+                        setChartData={setChartData}
                     />
                 </div>
+                {
+                    goToTransaction
+                    &&
+                    <Transaction
+                        currentCategory={currentCategory}
+                        setGotToTransaction={setGotToTransaction}
+                        transactionList={transactionList}
+                        setTransactionList={setTransactionList}
+                    />
+                }
+                <div className="budget_bottom">
+                    <span>transaction history</span>
+                    <span className="bugdet_transaction_smallnote">create expense categories like: Food, Rent, Saving etc.</span>
+                    <div className="transaction_list-heading_container" >
+                        <span>category</span>
+                        <span>date</span>
+                        <span>note</span>
+                        <span>amount</span>
+                    </div>
+                    <div className="budget_bottom-inner">
+                        {
+                            transactionList?.map(item => <TransactionList key={item?.id} item={item}/>)
+                        }
+                    </div>
+                </div>
             </div>
-            {
-                goToTransaction
-                &&
-                <Transaction
-                    currentCategory={currentCategory}
-                    setGotToTransaction={setGotToTransaction}
-                    transactionList={transactionList}
-                    setTransactionList={setTransactionList}
-                />
-            }
-            {
-                transactionList?.map(item => <TransactionList key={item?.id}/>)
-            }
         </div>
     )
 }
