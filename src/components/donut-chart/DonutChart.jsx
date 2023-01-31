43dcb390f9c6416940e0639a995db97004e2f7a6
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-01-31 06:33:27 +0530

    Update DonutChart.jsx

--- src/components/donut-chart/DonutChart.jsx
+++ src/components/donut-chart/DonutChart.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Chart as ChartJS, ArcElement, PieController, Legend, Tooltip} from 'chart.js';
+import { Pie } from 'react-chartjs-2';
 
-const DonutChart = () => {
+ChartJS.register(ArcElement, PieController,  Legend, Tooltip);
+
+
+const DonutChart = ({ data }) => {
+
+    React.useEffect(()=> {}, [data])
+
     return (
-        <div className="donut_chart">
-            Donut chart
+        <div className="donut_chart" style={{display:"flex", alignItems:'center', justifyContent:'center', padding:0}}>
+            <Pie data={data} options={{layout:{padding:20}}} />
         </div>
     )
 }
