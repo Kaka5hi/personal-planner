c13377d74f580cbd209d784739a1424177e2fe6d
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-02-22 08:32:38 +0530

    Update RecentTask.jsx

--- src/components/recent-task/RecentTask.jsx
+++ src/components/recent-task/RecentTask.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import './RecentTask.css'
 import {BsGridFill, BsListUl} from 'react-icons/bs'
 import SingleTask from '../single-task/SingleTask'
+import GridView from '../grid-view/GridView'
 
 const RecentTask = () => {
 
+    // create 3 different component here ->  
+    // 1. for no projectData
+    // 2. for grid view
+    // 3. for list view
     const [changeView, setChangeView] = React.useState(false)
-
+    const [localProjectData, setLocalProjectData] = React.useState(JSON.parse(localStorage.getItem('boardList')) || [])
 
     return (
         <div className='recent-task_main-container'>
@@ -15,12 +20,20 @@ const RecentTask = () => {
                 <button onClick={() => setChangeView(true)} style={changeView ? {backgroundColor:'black', color: 'white'} : {}} title='List View'><BsListUl/>list view</button>
             </div>
             <div style={changeView ? {gridTemplateColumns: '1fr'} : {gridTemplateColumns: 'repeat(2, 1fr)'}} className="recent-task_inner-container">
-                
-                <SingleTask />
-                <SingleTask />
-                <SingleTask />
-                <SingleTask />
+                {
+                    changeView
+                        ? localProjectData?.map(board => <SingleTask key={board?.id} item={board} />)
+                        : localProjectData?.map(board => <GridView key={board?.id} item={board} />)
+                }
             </div>
+            {
+                (localProjectData?.length === 0)
+                &&
+                <div className='no-data-section'>
+                    <h4>No projects</h4>
+                    <p>Go to Project section to create </p>
+                </div>
+            }
         </div>
     )
 }
