7c40e8bcd71978f25cd7a06d08f89f604d527a0f
Sushant Mehra <90246175+Kaka5hi@users.noreply.github.com>
2023-02-22 08:35:26 +0530

    Update Weather.jsx

--- src/components/weather-app/Weather.jsx
+++ src/components/weather-app/Weather.jsx
@@ -8,13 +8,12 @@ import {WiHumidity} from 'react-icons/wi'
 
 const Weather = () => {
 
-    const [latitude, setLatitude] = React.useState([])
-    const [longitude, setLongitude] = React.useState([])
+    const [latitude, setLatitude] = React.useState('')
+    const [longitude, setLongitude] = React.useState('')
     const [data, setData] = React.useState([])
     const [dataFetched, setDataFetched] = React.useState(false)
-    const [time, setTime] = React.useState(new Date().getHours())
-    const [date, setDate] = React.useState(new Date().toLocaleDateString())
-
+    const time = new Date().getHours()
+    const date = new Date().toLocaleDateString()
 
     React.useEffect(()=> {
         const getWeatherData = async() => {
@@ -25,7 +24,7 @@ const Weather = () => {
             try {
                 const resp = await fetch(`${process.env.REACT_APP_WEATHER_API_URL}/weather/?lat=${latitude}&lon=${longitude}&units=metric&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`)
                 const result = await resp.json()
-                if(result.cod === 200) {
+                if(result?.cod === 200) {
                     setData(result)
                     setDataFetched(true)
                 } else {
@@ -43,7 +42,7 @@ const Weather = () => {
         return (
             <div className='weather-app_container'>
                 <div className="widget-container">
-                    <div className="widget-inner_container">
+                    <div className="widget-inner_container" style={{display:'flex', alignItems: 'center', justifyContent: 'center', background:'rgba(0, 0, 0, 0.5)'}}>
                         <h1 style={{fontSize: '2.25em', fontWeight: 400, textTransform: 'capitalize', textAlign: 'center'}}>loading. . .</h1>
                     </div>
                 </div>
@@ -54,20 +53,20 @@ const Weather = () => {
         <div className='weather-app_container' style={(time >= 6 && time < 18) ? {background: 'url(https://img.freepik.com/free-photo/seoraksan-mountains-is-covered-by-morning-fog-sunrise-seoul-korea_335224-313.jpg?w=2000)', backgroundSize: 'cover'} : {background: 'url(https://i.pinimg.com/564x/85/d7/cb/85d7cb14bc1f65be306cd7695e66633a.jpg)',  backgroundSize: 'cover'}}>
             <div className="widget-container">
                 <div className="widget-inner_container">
-                    <h1>{data.main.temp}°<span>C</span></h1>
-                    <span>Max: {Math.ceil(data.main.temp_max)}°c</span>
-                    <span>Min: {Math.floor(data.main.temp_min)}°c</span>
+                    <h1>{data?.main.temp}°<span>C</span></h1>
+                    <span>Max: {Math.ceil(data?.main.temp_max)}°c</span>
+                    <span>Min: {Math.floor(data?.main.temp_min)}°c</span>
                 </div>
                 <div className="widget-inner_container">
                     <span>{(time >= 6 && time < 18) ? <MdOutlineWbSunny/> : <FaRegMoon/>}{(time >= 6 && time < 18) ? 'Day Time' : 'Night Time'} </span>
-                    <span><WiHumidity/> Humidity: {data.main.humidity}%</span>
-                    <span><BsWind/> Wind: {data.wind.speed}m/sec</span>
+                    <span><WiHumidity/> Humidity: {data?.main.humidity}%</span>
+                    <span><BsWind/> Wind: {data?.wind.speed}m/sec</span>
                     
                 </div>
                 <div className="widget-inner_container">
                     <span>today: {date}</span>
-                    <span><MdLocationPin/> {data.name}, {data.sys.country}</span>
-                    <span>{data.weather[0].description}</span>
+                    <span><MdLocationPin/> {data?.name}, {data?.sys.country}</span>
+                    <span>{data?.weather[0].description}</span>
                 </div>
             </div>
         </div>
